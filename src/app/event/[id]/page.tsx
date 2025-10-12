dba58c7f7e07d7430249223443cfd4b561dd5ca6
Gradi Puata <gradipuata@gmail.com>
2025-10-12 11:56:29 +0200

    Fix dynamic route

--- src/app/event/[id]/page.tsx
+++ src/app/event/[id]/page.tsx
@@ -6,7 +6,7 @@ import React from 'react'
 interface Props {
   params: Promise<{id:string}>
 }
-export const dynamic = 'force-dynamic'
+export const dynamic = "force-dynamic";
 
 type EventCategory = 'BEHIND_SCENES' | 'TOURNAMENTS' | 'LEAGUES';
 
